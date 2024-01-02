d9c2b8c5377e9b44a00f52c5d38daa6d345604ae
Adriswara <chevellerz454@yahoo.com>
2024-01-02 13:33:12 +0700

    changed table

--- table.php
+++ table.php
@@ -5,33 +5,18 @@
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">Nama</th>
+          <th scope="col">Longitude</th>
+          <th scope="col">Latitude</th>
           <th colspan="2" scope="col">Action</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td><a href=""><button class="btn btn-outline-primary" >Edit</button></a></td>
-          <td><a href=""><button class="btn btn-outline-danger">Delete</button></a></td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-          <td><a href=""><button class="btn btn-outline-primary">Edit</button></a></td>
-          <td><a href=""><button class="btn btn-outline-danger">Delete</button></a></td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
+          <td>A</td>
+          <td>B</td>
+          <td>C</td>
           <td><a href=""><button class="btn btn-outline-primary">Edit</button></a></td>
           <td><a href=""><button class="btn btn-outline-danger">Delete</button></a></td>
         </tr>
