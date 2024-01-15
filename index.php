e18ea98be831a29acdc2090255e819b575b21a5a
Adriswara <chevellerz454@yahoo.com>
2024-01-15 14:50:23 +0700

    dropdown & js fetch

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include "template/header.php" ?>
 
    <!-- NAVBAR FORM -->
+   
 
   <nav class="navbar navbar-expand-lg  bg-primary" data-bs-theme="dark">
     <div class="container-fluid">
@@ -14,22 +15,38 @@
                 <p>Pilih Titik Mulai : </p>
             </li>
             <li class="nav-item ms-4">
-            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </select>          
+            <form >
+              <select class="form-select" id="routeStart" aria-label="Floating label select example">
+                  <option selected>Open this select menu</option>
+                  <?php
+                  include "connection.php";
+                  $query = "SELECT * FROM rute";
+                  $rute = mysqli_query($db_connection, $query);
+
+                  $i= 1;
+                  foreach ($rute as $data)  :
+                  ?>
+                  <option value="<?php echo $data['id'] ?>"><?php echo $data['nama'] ?></option>
+                  <?php endforeach ?>          
+              </select>
+            </form>
           </li>
           <li class="ms-4">
                 <p>Pilih Tujuan : </p>
           </li>
           <li class="nav-item ms-4">
-            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+            <select class="form-select" id="routeEnd" aria-label="Floating label select example">
                 <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <?php
+                include "connection.php";
+                $query = "SELECT * FROM rute";
+                $rute = mysqli_query($db_connection, $query);
+
+                $i= 1;
+                foreach ($rute as $data)  :
+                ?>
+                <option value="<?php echo $data['id'] ?>"><?php echo $data['nama'] ?></option>
+                <?php endforeach ?>          
             </select>          
         </ul>
         <ul></ul>
@@ -47,19 +64,69 @@
 
    
        
-            <!-- MAP -->
+          <!-- MAP -->
             
-            <div id="map" style="width:100%; height: 100vh"></div>
+          <div id="map" style="width:100%; height: 100vh"></div>
 	        <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"></script>
 	        <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
 
-            <!-- MAP -->
+          <!-- MAP -->
 
+
+          <!-- table -->
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nama</th>
+          <th scope="col">Longitude</th>
+          <th scope="col">Latitude</th>
+          <th colspan="2" scope="col">Action</th>
+        </tr>
+      </thead>
+
+      <?php
+        include "connection.php";
+        $query = "SELECT * FROM rute";
+        $rute = mysqli_query($db_connection, $query);
+
+        $i= 1;
+        foreach ($rute as $data)  :
+      ?>
+
+      <tbody>
+        <tr>
+          <th scope="row"><?php echo $i++; ?></th>
+          <td><?php echo $data['nama']?></td>
+          <td><?php echo $data['longitude']?></td>
+          <td><?php echo $data['latitude']?></td>
+          <td><a href="edit.php?id=<?=$data['id']?>"><button class="btn btn-outline-primary">Edit</button></a></td>
+          <td><a href="delete.php?id=<?=$data['id']?>"><button class="btn btn-outline-danger">Delete</button></a></td>
+        </tr>
+      </tbody>
+      <?php endforeach ?>
+
+
+    </table>
+    <!-- table -->
       
 
 
 </body>
 <script>
+
+  // getter
+
+  var e = document.getElementById("routeStart");
+  function onChange() {
+    var value = e.value;
+    var text = e.options[e.selectedIndex].text;
+    console.log(value, text);
+  }
+  e.onchange = onChange;
+  onChange();
+
+  // getter
 
 		var map = L.map('map').setView([-6.9175, 107.6191], 11);
 		mapLink = "<a href='http://openstreetmap.org'>OpenStreetMap</a>";
