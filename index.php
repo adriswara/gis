ad32d1b8f6b7b3715a37835fe4feb58b5fd2baa3
Adriswara <chevellerz454@yahoo.com>
2024-01-25 17:18:53 +0700

    minor changes

--- index.php
+++ index.php
@@ -50,12 +50,7 @@
             </select>          
         </ul>
         <ul></ul>
-        <!-- <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-        </select> -->
+
       </div>
     </div>
   </nav>
@@ -74,7 +69,7 @@
 
 
           <!-- table -->
-    <table class="table">
+    <table class="table" style="display: none">
       <thead>
         <tr>
           <th scope="col">#</th>
